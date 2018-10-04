da4b3db1061cb8fdb882235ac32dd307328f0912
Михаил Михайлов <m.mikhailov@smartdriving.io>
2018-10-04 16:08:20 +0300

    Add base WPF solution with AmmyUI and Ui.Wpf.Common

---
+++ Domain0.Desktop/Domain0.Desktop/App.ammy.cs
@@ -0,0 +1,36 @@
+﻿using AmmySidekick;
+using System;
+using System.Windows;
+using Ui.Wpf.Common;
+using Ui.Wpf.Common.ShowOptions;
+
+namespace Domain0.Desktop
+{
+    public partial class App : Application
+    {
+        [STAThread]
+        public static void Main()
+        {
+            App app = new App();
+            app.InitializeComponent();
+
+            RuntimeUpdateHandler.Register(app, "/" + AmmySidekick.Ammy.GetAssemblyName(app) + ";component/App.g.xaml");
+
+            app.Run();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var shell = UiStarter.Start<IDockWindow>(
+                new Bootstrap(),
+                new UiShowStartWindowOptions
+                {
+                    Title = "Domain0.Desktop",
+                    ToolPaneWidth = 410
+                }
+            );
+        }
+    }
+}
